04b5d2af581af75f5976e3b932a082ebe9cfdee7
Micah Caunter <mcaunter12@gmail.com>
2021-07-10 14:00:56 -0400

    Another test

--- src/site/router/router-main.tsx
+++ src/site/router/router-main.tsx
@@ -13,6 +13,7 @@ import { Algorithms } from '../programming/mazes/algorithms/algorithms';
 import IsDev from '../../lib/helpers/check-environment';
 
 export const RouterMain: React.FC = () => {
+  alert(window.location.href);
   if (window.location.href.includes('/micahs-musings')) {
     return (
       <LayoutMain>
