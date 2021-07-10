1025cc2c683b9b1b14fc09182e1fe59611660123
Micah Caunter <mcaunter12@gmail.com>
2021-07-10 13:34:50 -0400

    Switching to Hash Router

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import * as React from 'react';
 import { ChakraProvider, theme } from '@chakra-ui/react';
 
 import { RouterMain } from './site/router/router-main';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import IsDev from './lib/helpers/check-environment';
 export const App = () => (
-  <BrowserRouter basename={IsDev() ? '/' : '/micahs-musings'}>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <ChakraProvider theme={theme}>
       <RouterMain />
     </ChakraProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
