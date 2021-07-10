2207da9e37baebaa6a5fad904edb547275725307
Micah Caunter <mcaunter12@gmail.com>
2021-07-10 13:45:19 -0400

    Back to Browser Router

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import * as React from 'react';
 import { ChakraProvider, theme } from '@chakra-ui/react';
 
 import { RouterMain } from './site/router/router-main';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import IsDev from './lib/helpers/check-environment';
 export const App = () => (
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter>
     <ChakraProvider theme={theme}>
       <RouterMain />
     </ChakraProvider>
-  </HashRouter>
+  </BrowserRouter>
 );
--- src/site/router/router-main.tsx
+++ src/site/router/router-main.tsx
@@ -10,27 +10,68 @@ import { TerrainGeneration } from '../programming/terrain-generation/terrain-gen
 import { Writing } from '../writing/writing';
 import { Generator } from '../programming/mazes/generator/generator';
 import { Algorithms } from '../programming/mazes/algorithms/algorithms';
+import IsDev from '../../lib/helpers/check-environment';
 
 export const RouterMain: React.FC = () => {
-  return (
-    <LayoutMain>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about-me" component={AboutMe} />
+  if (!IsDev) {
+    return (
+      <LayoutMain>
+        <Route exact path="/micahs-musings" component={Home} />
+        <Route exact path="/micahs-musings/about-me" component={AboutMe} />
 
-      <Route exact path="/programming" component={Programming} />
-      <Route exact path="/programming/mazes" component={Mazes} />
-      <Route exact path="/programming/mazes/generator" component={Generator} />
-      <Route
-        exact
-        path="/programming/mazes/algorithms"
-        component={Algorithms}
-      />
-      <Route
-        exact
-        path="/programming/terrain-generation"
-        component={TerrainGeneration}
-      />
-      <Route exact path="/writing" component={Writing} />
-    </LayoutMain>
-  );
+        <Route
+          exact
+          path="/micahs-musings/programming"
+          component={Programming}
+        />
+        <Route
+          exact
+          path="/micahs-musings/programming/mazes"
+          component={Mazes}
+        />
+        <Route
+          exact
+          path="/micahs-musings/programming/mazes/generator"
+          component={Generator}
+        />
+        <Route
+          exact
+          path="/micahs-musings/programming/mazes/algorithms"
+          component={Algorithms}
+        />
+        <Route
+          exact
+          path="/micahs-musings/programming/terrain-generation"
+          component={TerrainGeneration}
+        />
+        <Route exact path="/micahs-musings/writing" component={Writing} />
+      </LayoutMain>
+    );
+  } else {
+    return (
+      <LayoutMain>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about-me" component={AboutMe} />
+
+        <Route exact path="/programming" component={Programming} />
+        <Route exact path="/programming/mazes" component={Mazes} />
+        <Route
+          exact
+          path="/programming/mazes/generator"
+          component={Generator}
+        />
+        <Route
+          exact
+          path="/programming/mazes/algorithms"
+          component={Algorithms}
+        />
+        <Route
+          exact
+          path="/programming/terrain-generation"
+          component={TerrainGeneration}
+        />
+        <Route exact path="/writing" component={Writing} />
+      </LayoutMain>
+    );
+  }
 };
