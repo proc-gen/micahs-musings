d0efb8656e93e5582cf03e733e67e07a9faefdf6
Micah Caunter <mcaunter12@gmail.com>
2021-07-10 14:07:11 -0400

    Another test

--- src/site/router/left-nav.tsx
+++ src/site/router/left-nav.tsx
@@ -15,16 +15,13 @@ import { Link as ReactLink } from 'react-router-dom';
 export interface ILeftNavProps {}
 
 export const LeftNav: React.FC<ILeftNavProps> = () => {
-  const prefix = window.location.href.includes('/micahs-musings')
-    ? '/micahs-musings'
-    : '/';
   return (
     <SimpleGrid>
       <VStack align="stretch">
         <Accordion allowMultiple>
           <AccordionItem>
             <AccordionButton>
-              <Link as={ReactLink} to={prefix}>
+              <Link as={ReactLink} to="/">
                 Home
               </Link>
             </AccordionButton>
@@ -37,7 +34,7 @@ export const LeftNav: React.FC<ILeftNavProps> = () => {
             <AccordionPanel w="100%">
               <AccordionItem>
                 <AccordionButton>
-                  <Link as={ReactLink} to={prefix + '/programming'}>
+                  <Link as={ReactLink} to="/programming">
                     Overview
                   </Link>
                 </AccordionButton>
@@ -50,27 +47,21 @@ export const LeftNav: React.FC<ILeftNavProps> = () => {
                 <AccordionPanel w="100%">
                   <AccordionItem>
                     <AccordionButton>
-                      <Link as={ReactLink} to={prefix + '/programming/mazes'}>
+                      <Link as={ReactLink} to="/programming/mazes">
                         Overview
                       </Link>
                     </AccordionButton>
                   </AccordionItem>
                   <AccordionItem>
                     <AccordionButton>
-                      <Link
-                        as={ReactLink}
-                        to={prefix + '/programming/mazes/generator'}
-                      >
+                      <Link as={ReactLink} to="/programming/mazes/generator">
                         Generator
                       </Link>
                     </AccordionButton>
                   </AccordionItem>
                   <AccordionItem>
                     <AccordionButton>
-                      <Link
-                        as={ReactLink}
-                        to={prefix + '/programming/mazes/algorithms'}
-                      >
+                      <Link as={ReactLink} to="/programming/mazes/algorithms">
                         Algorithms
                       </Link>
                     </AccordionButton>
@@ -79,10 +70,7 @@ export const LeftNav: React.FC<ILeftNavProps> = () => {
               </AccordionItem>
               <AccordionItem>
                 <AccordionButton w="100%">
-                  <Link
-                    as={ReactLink}
-                    to={prefix + '/programming/terrain-generation'}
-                  >
+                  <Link as={ReactLink} to="/programming/terrain-generation">
                     Terrain Generation
                   </Link>
                 </AccordionButton>
@@ -96,7 +84,7 @@ export const LeftNav: React.FC<ILeftNavProps> = () => {
             </AccordionButton>
             <AccordionPanel w="100%">
               <AccordionButton>
-                <Link as={ReactLink} to={prefix + '/writing'} w="100%">
+                <Link as={ReactLink} to="/writing" w="100%">
                   Overview
                 </Link>
               </AccordionButton>
@@ -104,7 +92,7 @@ export const LeftNav: React.FC<ILeftNavProps> = () => {
           </AccordionItem>
           <AccordionItem>
             <AccordionButton>
-              <Link as={ReactLink} to={prefix + '/about-me'}>
+              <Link as={ReactLink} to="/about-me">
                 About Me
               </Link>
             </AccordionButton>
--- src/site/router/router-main.tsx
+++ src/site/router/router-main.tsx
@@ -13,65 +13,25 @@ import { Algorithms } from '../programming/mazes/algorithms/algorithms';
 import IsDev from '../../lib/helpers/check-environment';
 
 export const RouterMain: React.FC = () => {
-  if (window.location.href.includes('/micahs-musings')) {
-    return (
-      <LayoutMain>
-        <Route exact path="/micahs-musings" component={Home} />
-        <Route exact path="/micahs-musings/about-me" component={AboutMe} />
+  return (
+    <LayoutMain>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about-me" component={AboutMe} />
 
-        <Route
-          exact
-          path="/micahs-musings/programming"
-          component={Programming}
-        />
-        <Route
-          exact
-          path="/micahs-musings/programming/mazes"
-          component={Mazes}
-        />
-        <Route
-          exact
-          path="/micahs-musings/programming/mazes/generator"
-          component={Generator}
-        />
-        <Route
-          exact
-          path="/micahs-musings/programming/mazes/algorithms"
-          component={Algorithms}
-        />
-        <Route
-          exact
-          path="/micahs-musings/programming/terrain-generation"
-          component={TerrainGeneration}
-        />
-        <Route exact path="/micahs-musings/writing" component={Writing} />
-      </LayoutMain>
-    );
-  } else {
-    return (
-      <LayoutMain>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about-me" component={AboutMe} />
-
-        <Route exact path="/programming" component={Programming} />
-        <Route exact path="/programming/mazes" component={Mazes} />
-        <Route
-          exact
-          path="/programming/mazes/generator"
-          component={Generator}
-        />
-        <Route
-          exact
-          path="/programming/mazes/algorithms"
-          component={Algorithms}
-        />
-        <Route
-          exact
-          path="/programming/terrain-generation"
-          component={TerrainGeneration}
-        />
-        <Route exact path="/writing" component={Writing} />
-      </LayoutMain>
-    );
-  }
+      <Route exact path="/programming" component={Programming} />
+      <Route exact path="/programming/mazes" component={Mazes} />
+      <Route exact path="/programming/mazes/generator" component={Generator} />
+      <Route
+        exact
+        path="/programming/mazes/algorithms"
+        component={Algorithms}
+      />
+      <Route
+        exact
+        path="/programming/terrain-generation"
+        component={TerrainGeneration}
+      />
+      <Route exact path="/writing" component={Writing} />
+    </LayoutMain>
+  );
 };
