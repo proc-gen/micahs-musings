d06dad6d775d6c63f5256d0136abb6d871a12c42
Micah Caunter <mcaunter12@gmail.com>
2021-07-10 13:27:27 -0400

    Fixing routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import { ChakraProvider, theme } from '@chakra-ui/react';
 
 import { RouterMain } from './site/router/router-main';
 import { BrowserRouter } from 'react-router-dom';
-
+import IsDev from './lib/helpers/check-environment';
 export const App = () => (
-  <BrowserRouter>
+  <BrowserRouter basename={IsDev() ? '/' : '/micahs-musings'}>
     <ChakraProvider theme={theme}>
       <RouterMain />
     </ChakraProvider>
