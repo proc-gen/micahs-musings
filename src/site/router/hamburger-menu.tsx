911776cdd1b1247d57dc8cf477be7e3fa0cbec6c
Micah Caunter <mcaunter12@gmail.com>
2021-07-17 15:53:17 -0400

    Added hamburger menu to show/hide the left nav

---
+++ src/site/router/hamburger-menu.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { IconButton, IconButtonProps } from '@chakra-ui/react';
+import { FaBars } from 'react-icons/fa';
+
+type HamburgerMenuProps = Omit<IconButtonProps, 'aria-label'>;
+interface IHamburgerMenuProps extends HamburgerMenuProps {
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  menuOpen: boolean;
+}
+
+export const HamburgerMenu: React.FC<IHamburgerMenuProps> = (props) => {
+  return (
+    <IconButton
+      size="md"
+      fontSize="lg"
+      variant="ghost"
+      color="current"
+      onClick={props.onClick}
+      icon={<FaBars />}
+      aria-label={props.menuOpen ? 'Close left nav' : 'Open left nav'}
+      {...props}
+    />
+  );
+};
--- src/site/router/layout-main.tsx
+++ src/site/router/layout-main.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 
 import {
   Box,
@@ -10,40 +10,99 @@ import {
 } from '@chakra-ui/react';
 
 import { ColorModeSwitcher } from './color-mode-switcher';
+import { HamburgerMenu } from './hamburger-menu';
 import { LeftNav } from './left-nav';
 
-export const LayoutMain: React.FC = ({ children }) => {
+interface ILayoutMainState {
+  menuOpen: boolean;
+}
+
+const PageLayout: React.FC = ({ children, ...rest }) => {
   return (
     <Box
       textAlign="center"
       fontSize="xl"
       backgroundColor={useColorModeValue('gray.100', 'gray.900')}
+      {...rest}
     >
-      <SimpleGrid
-        paddingBottom="1em"
-        rows={1}
-        minChildWidth="5em"
-        backgroundColor={useColorModeValue('gray.200', 'gray.800')}
-      >
-        <GridItem textAlign="left">
-          <Heading size="xl" paddingLeft=".5em">
-            Micah's Musings
-          </Heading>
-        </GridItem>
-        <GridItem textAlign="right">
-          <ColorModeSwitcher justifySelf="flex-end" />
-        </GridItem>
-      </SimpleGrid>
-
-      <Grid minH="100vh" templateColumns="repeat(12, 1fr)">
-        <GridItem
-          colSpan={2}
-          backgroundColor={useColorModeValue('gray.200', 'gray.800')}
-        >
-          <LeftNav />
-        </GridItem>
-        <GridItem colSpan={10}>{children}</GridItem>
-      </Grid>
+      {children}
     </Box>
   );
 };
+
+interface ITopNavProps {
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  menuOpen: boolean;
+}
+
+const TopNav: React.FC<ITopNavProps> = ({ onClick, menuOpen, ...rest }) => {
+  return (
+    <SimpleGrid
+      paddingBottom="1em"
+      rows={1}
+      minChildWidth="5em"
+      backgroundColor={useColorModeValue('gray.200', 'gray.800')}
+    >
+      <GridItem textAlign="left">
+        <HamburgerMenu menuOpen={menuOpen} onClick={onClick} />
+      </GridItem>
+      <GridItem textAlign="center">
+        <Heading size="xl">Micah's Musings</Heading>
+      </GridItem>
+      <GridItem textAlign="right">
+        <ColorModeSwitcher justifySelf="flex-end" />
+      </GridItem>
+    </SimpleGrid>
+  );
+};
+
+interface ILayoutProps {
+  children?: React.ReactNode;
+  menuOpen: boolean;
+}
+
+const MainLayout: React.FC<ILayoutProps> = ({ children, menuOpen }) => {
+  return (
+    <Grid minH="100vh" templateColumns="repeat(12, 1fr)">
+      <GridItem
+        colSpan={menuOpen ? 2 : 0}
+        display={menuOpen ? 'initial' : 'none'}
+        transition="visibility 0s, opacity 0.5s linear"
+        backgroundColor={useColorModeValue('gray.200', 'gray.800')}
+      >
+        <LeftNav />
+      </GridItem>
+      <GridItem colSpan={menuOpen ? 10 : 12} transition="0.5s">
+        {children}
+      </GridItem>
+    </Grid>
+  );
+};
+
+export class LayoutMain extends React.Component<any, ILayoutMainState> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = { menuOpen: false };
+    this.handleHamburgerClick = this.handleHamburgerClick.bind(this);
+  }
+
+  handleHamburgerClick(event: React.MouseEvent<HTMLButtonElement>) {
+    const { menuOpen } = this.state;
+    this.setState({ menuOpen: !menuOpen });
+  }
+
+  render() {
+    return (
+      <PageLayout>
+        <TopNav
+          menuOpen={this.state.menuOpen}
+          onClick={this.handleHamburgerClick}
+        />
+        <MainLayout menuOpen={this.state.menuOpen}>
+          {this.props.children}
+        </MainLayout>
+      </PageLayout>
+    );
+  }
+}
