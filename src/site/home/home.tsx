e0c525931660ecd99b68df21df869cc679aaf828
Micah Caunter <mcaunter12@gmail.com>
2021-07-18 10:13:51 -0400

    Minor update to home page

--- src/site/home/home.tsx
+++ src/site/home/home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SimpleGrid, Text } from '@chakra-ui/react';
+import { SimpleGrid, Text, Container } from '@chakra-ui/react';
 
 import {
   LinkButton,
@@ -17,28 +17,30 @@ import SpaceMaze from './images/3-d-maze.png';
 export const Home: React.FC = () => {
   return (
     <>
-      <SimpleGrid columns={2} spacing="5em" padding="1em">
-        <Thumbnail source={MoltenTeapot} altText="Molten Teapot" />
-        <Thumbnail source={NoiseTerrain} altText="Terrain Generation" />
-      </SimpleGrid>
-      <Card maxW="container.lg" centered>
-        <CardHeader>Welcome!</CardHeader>
-        <CardBody>
-          <Text>
-            I made this website to share things that I've worked on because
-            they're cool, interesting, or just meant to make you think. Enjoy!
-          </Text>
-          <SimpleGrid columns={2} spacing="1em" padding="1em">
-            <LinkButton href="/mazes" text="Mazes" />
-            <LinkButton href="/writing" text="Writing" />
-          </SimpleGrid>
-        </CardBody>
-      </Card>
+      <Container maxW="container.lg">
+        <SimpleGrid columns={2} spacing="5em" padding="1em">
+          <Thumbnail source={MoltenTeapot} altText="Molten Teapot" />
+          <Thumbnail source={NoiseTerrain} altText="Terrain Generation" />
+        </SimpleGrid>
+        <Card maxW="container.lg" centered>
+          <CardHeader>Welcome!</CardHeader>
+          <CardBody>
+            <Text>
+              I made this website to share things that I've worked on because
+              they're cool, interesting, or just meant to make you think. Enjoy!
+            </Text>
+            <SimpleGrid columns={2} spacing="1em" padding="1em">
+              <LinkButton href="/mazes" text="Mazes" />
+              <LinkButton href="/writing" text="Writing" />
+            </SimpleGrid>
+          </CardBody>
+        </Card>
 
-      <SimpleGrid columns={2} spacing="5em" padding="1em">
-        <Thumbnail source={MazeGenerator} altText="Maze Generator" />
-        <Thumbnail source={SpaceMaze} altText="3D Maze" />
-      </SimpleGrid>
+        <SimpleGrid columns={2} spacing="5em" padding="1em">
+          <Thumbnail source={MazeGenerator} altText="Maze Generator" />
+          <Thumbnail source={SpaceMaze} altText="3D Maze" />
+        </SimpleGrid>
+      </Container>
     </>
   );
 };
