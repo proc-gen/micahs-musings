03678857871857f519d915c2217b8f507e9f1097
Micah Caunter <mcaunter12@gmail.com>
2021-07-21 07:15:08 -0400

    Added direction bias properties to growing tree from recursive backtracker

--- src/lib/mazes/generators/growing-tree.ts
+++ src/lib/mazes/generators/growing-tree.ts
@@ -8,11 +8,33 @@ export class GrowingTreeData {
   startPosY: number;
   treeGrammar: string;
 
+  //Directional bias can be immplemented using either the cardinal directions
+  // or the direction to turn
+  directionBias: 'Cardinal' | 'Turn';
+  northChance: number;
+  eastChance: number;
+  southChance: number;
+  westChance: number;
+
+  forwardChance: number;
+  leftChance: number;
+  rightChance: number;
+
   constructor() {
     this.setStartPosition = false;
     this.startPosX = 0;
     this.startPosY = 0;
     this.treeGrammar = 'R';
+
+    this.directionBias = 'Cardinal';
+    this.northChance = 25;
+    this.eastChance = 25;
+    this.southChance = 25;
+    this.westChance = 25;
+
+    this.forwardChance = 34;
+    this.leftChance = 33;
+    this.rightChance = 33;
   }
 }
 
@@ -30,8 +52,8 @@ export class GrowingTree extends Generator {
     let currentCell: Cell;
     let nextCell: Cell | undefined;
     let pickCount: number;
-    let adjacentCount: number;
     let grammarPosition: number = 0;
+    let firstCell: Cell;
 
     for (let i: number = 0; i < this.baseProps.width; i++) {
       for (let j: number = 0; j < this.baseProps.height; j++) {
@@ -57,7 +79,7 @@ export class GrowingTree extends Generator {
     } else {
       currentCell = mapCells[this.random.GetInt(mapCells.length)];
     }
-
+    firstCell = currentCell;
     currentCell.visited = true;
     usedCells.push(currentCell);
     do {
@@ -86,16 +108,143 @@ export class GrowingTree extends Generator {
       } while (
         !currentCell.adjacentCells.some((a) => a !== undefined && !a.visited)
       );
+
+      let cellIndex: number = -1;
+      let cellDist: number = 9999;
+      for (let i: number = 0; i < currentCell.numCellSides; i++) {
+        if (
+          currentCell.adjacentCells[i] !== undefined &&
+          (currentCell.adjacentCells[i] as Cell).visited
+        ) {
+          if (cellIndex === -1) {
+            cellIndex = i;
+            cellDist =
+              Math.pow(
+                (currentCell.adjacentCells[i] as Cell).x - firstCell.x,
+                2
+              ) +
+              Math.pow(
+                (currentCell.adjacentCells[i] as Cell).y - firstCell.y,
+                2
+              );
+          } else {
+            let newCellDist: number =
+              Math.pow(
+                (currentCell.adjacentCells[i] as Cell).x - firstCell.x,
+                2
+              ) +
+              Math.pow(
+                (currentCell.adjacentCells[i] as Cell).y - firstCell.y,
+                2
+              );
+            if (newCellDist < cellDist) {
+              cellDist = newCellDist;
+              cellIndex = i;
+            }
+          }
+        }
+      }
+      nextCell = this.pickNextCell(
+        currentCell,
+        currentCell.adjacentCells[cellIndex]
+      );
+
       grammarPosition = (grammarPosition + 1) % this.props.treeGrammar.length;
-      adjacentCount = currentCell.adjacentCells.filter(
-        (a) => a !== undefined && !a.visited
-      ).length;
-      nextCell = currentCell.adjacentCells.filter(
-        (a) => a !== undefined && !a.visited
-      )[this.random.GetInt(adjacentCount)];
+
       (nextCell as Cell).visited = true;
       this.MergeCells(currentCell, nextCell);
       usedCells.push(nextCell as Cell);
     } while (mapCells.some((a) => !a.visited));
+  }
+
+  pickNextCell(currentCell: Cell, previousCell: Cell | undefined): Cell {
+    let nextCell: Cell;
+    let randomMax: number = 0;
+    const adjacentCells = currentCell.adjacentCells.filter(
+      (a) => a !== undefined && !a.visited
+    );
+
+    if (this.props.directionBias === 'Cardinal') {
+      for (let i: number = 0; i < currentCell.adjacentCells.length; i++) {
+        if (
+          currentCell.adjacentCells[i] !== undefined &&
+          !(currentCell.adjacentCells[i] as Cell).visited
+        )
+          switch (i) {
+            case 0:
+              randomMax += this.props.northChance;
+              break;
+            case 1:
+              randomMax += this.props.eastChance;
+              break;
+            case 2:
+              randomMax += this.props.southChance;
+              break;
+            case 3:
+              randomMax += this.props.westChance;
+              break;
+          }
+      }
+
+      nextCell = adjacentCells[
+        this.random.GetInt(randomMax) % adjacentCells.length
+      ] as Cell;
+    } else {
+      if (adjacentCells.length === 1) {
+        nextCell = adjacentCells[0] as Cell;
+      } else if (previousCell !== undefined) {
+        let previousCellIndex = currentCell.adjacentCells.indexOf(previousCell);
+        let forwardIndex = (previousCellIndex + 2) % 4;
+        let forwardMax = 0;
+        let leftIndex = (previousCellIndex + 1) % 4;
+        let leftMax = 0;
+        let rightIndex = (previousCellIndex + 3) % 4;
+        //let rightMax = 0;
+
+        if (
+          currentCell.adjacentCells[forwardIndex] !== undefined &&
+          !(currentCell.adjacentCells[forwardIndex] as Cell).visited
+        ) {
+          randomMax += this.props.forwardChance;
+          forwardMax = this.props.forwardChance - 1;
+        }
+        if (
+          currentCell.adjacentCells[leftIndex] !== undefined &&
+          !(currentCell.adjacentCells[leftIndex] as Cell).visited
+        ) {
+          randomMax += this.props.leftChance;
+          leftMax = randomMax + this.props.leftChance - 1;
+        }
+        if (
+          currentCell.adjacentCells[rightIndex] !== undefined &&
+          !(currentCell.adjacentCells[rightIndex] as Cell).visited
+        ) {
+          randomMax += this.props.rightChance;
+          //rightMax = randomMax + this.props.rightChance - 1;
+        }
+
+        let randomValue = this.random.GetInt(randomMax);
+
+        if (randomValue <= forwardMax) {
+          nextCell = currentCell.adjacentCells[forwardIndex] as Cell;
+        } else if (
+          (forwardMax === 0 || randomValue > forwardMax) &&
+          randomValue <= leftMax
+        ) {
+          nextCell = currentCell.adjacentCells[leftIndex] as Cell;
+        } /*if (
+          (leftMax === 0 || randomValue > leftMax) &&
+          randomValue <= rightMax
+        ) */ else {
+          nextCell = currentCell.adjacentCells[rightIndex] as Cell;
+        }
+      } else {
+        nextCell = adjacentCells[
+          this.random.GetInt(adjacentCells.length)
+        ] as Cell;
+      }
+    }
+
+    return nextCell;
   }
 }
--- src/site/mazes/generator/components/growing-tree-properties.tsx
+++ src/site/mazes/generator/components/growing-tree-properties.tsx
@@ -81,6 +81,103 @@ export class GrowingTreeProperties extends React.Component<
             </ul>
           </Text>
         </SimpleGrid>
+        <SimpleGrid columns={3} spacing="1em" paddingBottom="1em">
+          <InputSelect
+            id="directionBias"
+            label="Direction Bias"
+            tooltip="Sets whether the direction bias should be based on the cardinal directions or based on direction compared to the previous cell."
+            value={this.props.data.directionBias.toString()}
+            onChange={this.handleSelectChange}
+          >
+            <option value="Cardinal">Cardinal</option>
+            <option value="Turn">Turn</option>
+          </InputSelect>
+        </SimpleGrid>
+        <SimpleGrid
+          columns={4}
+          spacing="1em"
+          paddingBottom="1em"
+          visibility={
+            this.props.data.directionBias === 'Cardinal' ? 'initial' : 'hidden'
+          }
+          display={
+            this.props.data.directionBias === 'Cardinal' ? 'grid' : 'none'
+          }
+        >
+          <InputText
+            id="northChance"
+            label="North Chance"
+            tooltip="The percentage chance that North will be chosen."
+            placeholder="North Chance"
+            value={this.props.data.northChance}
+            onChange={this.handleInputChange}
+            rightAddon="%"
+          />
+          <InputText
+            id="eastChance"
+            label="East Chance"
+            tooltip="The percentage chance that East will be chosen."
+            placeholder="East Chance"
+            value={this.props.data.eastChance}
+            onChange={this.handleInputChange}
+            rightAddon="%"
+          />
+          <InputText
+            id="southChance"
+            label="South Chance"
+            tooltip="The percentage chance that South will be chosen."
+            placeholder="South Chance"
+            value={this.props.data.southChance}
+            onChange={this.handleInputChange}
+            rightAddon="%"
+          />
+          <InputText
+            id="westChance"
+            label="West Chance"
+            tooltip="The percentage chance that West will be chosen."
+            placeholder="West Chance"
+            value={this.props.data.westChance}
+            onChange={this.handleInputChange}
+            rightAddon="%"
+          />
+        </SimpleGrid>
+        <SimpleGrid
+          columns={3}
+          spacing="1em"
+          paddingBottom="1em"
+          visibility={
+            this.props.data.directionBias === 'Turn' ? 'initial' : 'hidden'
+          }
+          display={this.props.data.directionBias === 'Turn' ? 'grid' : 'none'}
+        >
+          <InputText
+            id="forwardChance"
+            label="Forward Chance"
+            tooltip="The percentage chance that moving forward will be chosen."
+            placeholder="Forward Chance"
+            value={this.props.data.forwardChance}
+            onChange={this.handleInputChange}
+            rightAddon="%"
+          />
+          <InputText
+            id="leftChance"
+            label="Left Chance"
+            tooltip="The percentage chance that a left turn will be chosen."
+            placeholder="Left Chance"
+            value={this.props.data.leftChance}
+            onChange={this.handleInputChange}
+            rightAddon="%"
+          />
+          <InputText
+            id="rightChance"
+            label="Right Chance"
+            tooltip="The percentage chance that a right turn will be chosen."
+            placeholder="Right Chance"
+            value={this.props.data.rightChance}
+            onChange={this.handleInputChange}
+            rightAddon="%"
+          />
+        </SimpleGrid>
       </>
     );
   }
